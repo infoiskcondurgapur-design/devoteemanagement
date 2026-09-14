6c41a39bb864677664e98056e94182716ddafab9
edpdurgapur <maji.abhijit.maji@gmail.com>
2026-09-14 12:47:43 +0530

    Fix rootDir calculation in migration script

--- api/_src/scripts/migrate.mjs
+++ api/_src/scripts/migrate.mjs
@@ -24,7 +24,7 @@ import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const rootDir = join(__dirname, '..', '..');
+const rootDir = join(__dirname, '..', '..', '..');
 
 import { INIT_DDL, SETTINGS_SEEDS } from '../schema.mjs';
 import pg from 'pg';
